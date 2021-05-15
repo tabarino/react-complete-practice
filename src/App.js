b6df404505d8c31e807865c1cba1fbac10bea471
Ivan Tabarino <tabarino@outlook.com>
2021-05-15 14:19:25 +0100

    Add an UsersList Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import AddUser from './components/Users/AddUser/AddUser';
+import UsersList from './components/Users/UsersList/UsersList';
 
 function App() {
   return (
     <div className="App">
       <AddUser />
+      <UsersList users={[]} />
     </div>
   );
 }
---
+++ src/components/Users/UsersList/UsersList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Card from '../../UserInterface/Card/Card';
+import styles from './UsersList.module.css';
+
+const UsersList = (props) => {
+  return (
+    <Card className={styles.users}>
+      <ul>
+        {props.users.map(user => {
+          <li>{user.name} ({user.age} years old)</li>
+        })}
+      </ul>
+    </Card>
+  );
+}
+
+export default UsersList;
